5915a983ebdc0e6439349968246453e32154361f
Hamza Lamkhailif <lamkhailifhamza@gmail.com>
2025-02-23 18:32:44 +0100

    Fixed line endings in .gitignore

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+      {/* Put your navbar here */}
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {/* Put your HomePage here */}
+    </div>
+  );
+};
+
+export default HomePage;
